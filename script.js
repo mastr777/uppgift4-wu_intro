017074387976075cc3e3f8f86c784b867d67c103
mastr777 <mstrandberg777@gmail.com>
2025-09-19 00:08:22 +0200

    almost ready for the next steps in .js

--- script.js
+++ script.js
@@ -2,49 +2,65 @@
 ToDo List js
  */
 
-/* let textInput;
-let taskList = "<ol>";
-let grabText; */
+// declaration of needed variables
 
 const completedTasks = document.querySelector("#completedTasks");
 const textInput = document.querySelector("#textInput");
 const grabTextBtn = document.querySelector("#grabTextBtn");
 const infoText = document.querySelector("small")
-//const taskList = document.querySelector("#taskList");
 const todoList = document.querySelector("#todoList");
+
 
 let todoText = "";
 let completed = 0;
 
 // event listner for button Click
-
 grabTextBtn.addEventListener("click", addToList);
 
 
+// function start, when clicked button ---------------------------------------------------------------
 // add text from Input to a list
-
 function addToList() {
+
+                    // completedTasks.textContent = "You have no active tasks at the moment";
 
 infoText.textContent = "";
 
 todoText = textInput.value;
 if (todoText.length == 0) {
 
-    infoText.textContent = "Please, type a task and press add !!";
+    infoText.textContent = "Please, type a task and press Add Task !!";
     return;
-
 }
 
-const listElement = document.createElement('li');
-todoList.appendChild(listElement);
+const item = document.createElement('li');
+todoList.appendChild(item);
 
-const wrappedTask = document.createElement('span');
-wrappedTask.innerText = todoText;
+const itemText = document.createElement('span');
+itemText.innerText = todoText;
 
-listElement.appendChild(wrappedTask);
+// added EventListener to span (text)
+itemText.addEventListener("click", 
+    function(){
+        
+        if (itemText.classList.contains("completed"))
+        {
+            itemText.setAttribute('class', "");
+            completed--;
+            completedTasks.textContent = "You have " + completed + " tasks done";
+        }
+        else 
+        {
+            itemText.setAttribute("class", "completed"); 
+            completed++;
+            completedTasks.textContent = "You have " + completed + " tasks done";
+        }
+    }
+)
+
+item.appendChild(itemText);
 
 // clear input field
-
 textInput.value = "";
 
 }

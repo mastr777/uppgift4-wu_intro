82da8d68c98fd462a829e0f9ddc56941af923ba4
mastr777 <mstrandberg777@gmail.com>
2025-09-22 23:39:17 +0200

    Added trashcan and changed some code in .js

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ ToDo List js
 
 // declaration of needed variables
 
-const completedTasks = document.querySelector("#completedTasks");
-const textInput = document.querySelector("#textInput");
-const grabTextBtn = document.querySelector("#grabTextBtn");
-const infoText = document.querySelector("small")
-const todoList = document.querySelector("#todoList");
+const completedTasks = document.querySelector('#completedTasks');
 
-let todoText = "";
+const textInput = document.querySelector('#textInput');
+const grabTextBtn = document.querySelector('#grabTextBtn');
+const infoText = document.querySelector('small')
+const todoList = document.querySelector('#todoList');
+
+let todoText = '';
 let completed = 0;
 
 // event listner for button Click
-grabTextBtn.addEventListener("click", addToList);
+grabTextBtn.addEventListener('click', addToList);
 
 const todoArray = [];
 
@@ -24,56 +25,69 @@ const todoArray = [];
 // add text from Input to a list
 function addToList() {
 
-    infoText.textContent = "";
+    infoText.textContent = '';
 
     todoText = textInput.value;
 
     if (todoText.length == 0) {
 
-        infoText.textContent = "Please, type a task and press Add Task !!";
+        infoText.textContent = 'Please, type a task and press Add Task !!';
         return;
     }
 
-        // adding sent input into an Array
+        // adding input into an Array
         let text = textInput.value;
         todoArray.push(text);
 
     const item = document.createElement('li');
     todoList.appendChild(item);
     const itemText = document.createElement('span');
+    itemText.setAttribute('class', 'listText');
     itemText.innerText = todoText;
 
-    // --------------------------------------------------------------------------------------------------
-    // delete task
-    /* const deleteTask = document.createElement('img');
-    deleteTask.src ="test.jpg";
-    deleteTask.alt ="   DEL";
-    deleteTask.width = 5;
-    deleteTask.height = 5;
-    itemText.appendChild(deleteTask); */
-    // --------------------------------------------------------------------------------------------------
+    // trashcan code for an appearing span element
+    const trashcan = document.createElement('span');
+    trashcan.innerHTML = '&#x1F5D1';
+    trashcan.setAttribute('class', 'trashcan');
+
+    trashcan.addEventListener('click', function() {
+        
+        if (itemText.classList.contains('completed')) {
+                itemText.setAttribute('class', 'listText');
+                completed--;
+                completedTasks.textContent = 'You have ' + completed + ' tasks done';
+                item.remove();
+            }
+            else {
+
+            item.remove();
+
+            }
+    }
+    );
 
 
     // added EventListener to span (text)
-    itemText.addEventListener("click",
+    itemText.addEventListener('click',
         function () {
 
-            if (itemText.classList.contains("completed")) {
-                itemText.setAttribute('class', "");
+            if (itemText.classList.contains('completed')) {
+                itemText.setAttribute('class', 'listText');
                 completed--;
-                completedTasks.textContent = "You have " + completed + " tasks done";
+                completedTasks.textContent = 'You have ' + completed + ' tasks done';
             }
             else {
-                itemText.setAttribute("class", "completed");
+                itemText.setAttribute('class', 'completed');
                 completed++;
-                completedTasks.textContent = "You have " + completed + " tasks done";
+                completedTasks.textContent = 'You have ' + completed + ' tasks done';
             }
         }
     )
 
     item.appendChild(itemText);
+    item.appendChild(trashcan);
 
     // clear input field
-    textInput.value = "";
+    textInput.value = '';
 
 }

07341302f4131222e305363eec3822d35eed4b81
mastr777 <mstrandberg777@gmail.com>
2025-09-22 21:57:49 +0200

    Added grab text to an array with push in .js

--- script.js
+++ script.js
@@ -16,60 +16,64 @@ let completed = 0;
 // event listner for button Click
 grabTextBtn.addEventListener("click", addToList);
 
+const todoArray = [];
+
 
 // function start, when clicked button --------------------------------------------------------------
+
 // add text from Input to a list
 function addToList() {
 
-                    // completedTasks.textContent = "You have no active tasks at the moment";
+    infoText.textContent = "";
 
-infoText.textContent = "";
+    todoText = textInput.value;
 
-todoText = textInput.value;
-if (todoText.length == 0) {
+    if (todoText.length == 0) {
 
-    infoText.textContent = "Please, type a task and press Add Task !!";
-    return;
-}
-
-const item = document.createElement('li');
-todoList.appendChild(item);
-
-const itemText = document.createElement('span');
-itemText.innerText = todoText;
-
-// --------------------------------------------------------------------------------------------------
-// delete task
-/* const deleteTask = document.createElement('img');
-deleteTask.src ="test.jpg";
-deleteTask.alt ="   DEL";
-deleteTask.width = 5;
-deleteTask.height = 5;
-itemText.appendChild(deleteTask); */
-// --------------------------------------------------------------------------------------------------
-
-// added EventListener to span (text)
-itemText.addEventListener("click", 
-    function(){
-        
-        if (itemText.classList.contains("completed"))
-        {
-            itemText.setAttribute('class', "");
-            completed--;
-            completedTasks.textContent = "You have " + completed + " tasks done";
-        }
-        else 
-        {
-            itemText.setAttribute("class", "completed"); 
-            completed++;
-            completedTasks.textContent = "You have " + completed + " tasks done";
-        }
+        infoText.textContent = "Please, type a task and press Add Task !!";
+        return;
     }
-)
 
-item.appendChild(itemText);
+        // adding sent input into an Array
+        let text = textInput.value;
+        todoArray.push(text);
 
-// clear input field
-textInput.value = "";
+    const item = document.createElement('li');
+    todoList.appendChild(item);
+    const itemText = document.createElement('span');
+    itemText.innerText = todoText;
+
+    // --------------------------------------------------------------------------------------------------
+    // delete task
+    /* const deleteTask = document.createElement('img');
+    deleteTask.src ="test.jpg";
+    deleteTask.alt ="   DEL";
+    deleteTask.width = 5;
+    deleteTask.height = 5;
+    itemText.appendChild(deleteTask); */
+    // --------------------------------------------------------------------------------------------------
+
+
+    // added EventListener to span (text)
+    itemText.addEventListener("click",
+        function () {
+
+            if (itemText.classList.contains("completed")) {
+                itemText.setAttribute('class', "");
+                completed--;
+                completedTasks.textContent = "You have " + completed + " tasks done";
+            }
+            else {
+                itemText.setAttribute("class", "completed");
+                completed++;
+                completedTasks.textContent = "You have " + completed + " tasks done";
+            }
+        }
+    )
+
+    item.appendChild(itemText);
+
+    // clear input field
+    textInput.value = "";
 
 }
